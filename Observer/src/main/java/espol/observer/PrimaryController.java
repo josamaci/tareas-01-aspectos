bccb7629604810bb3f375a25657ef9a3de6e1cec
josamaci <84406998+josamaci@users.noreply.github.com>
2021-10-28 16:59:38 -0500

    creando interfaz

--- Observer/src/main/java/espol/observer/PrimaryController.java
+++ Observer/src/main/java/espol/observer/PrimaryController.java
@@ -2,11 +2,31 @@ package espol.observer;
 
 import java.io.IOException;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
 
 public class PrimaryController {
 
     @FXML
-    private void switchToSecondary() throws IOException {
-        App.setRoot("secondary");
+    private AnchorPane fondo;
+    @FXML
+    private Button btRojo;
+    @FXML
+    private Button btAzul;
+    @FXML
+    private Button btVerde;
+
+    @FXML
+    private void cambiarRojo(MouseEvent event) {
     }
+
+    @FXML
+    private void cambiarAzul(MouseEvent event) {
+    }
+
+    @FXML
+    private void cambiarVerde(MouseEvent event) {
+    }
+
 }
